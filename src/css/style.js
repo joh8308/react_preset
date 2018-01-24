5cc69ae1d95958a7a266e1fd7cf249d807a53669
john <joh8308>
2018-01-24 11:56:02 +0900

    first commit

--- src/css/style.js
+++
@@ -1,8 +0,0 @@
-import './bootstrap.css';
-import './layout-boxed.css';
-import './layout-semiboxed.css';
-import './layout-wide.css';
-import './responsive.css';
-import './style.css';
-import './App.css';
-import './index.css';
